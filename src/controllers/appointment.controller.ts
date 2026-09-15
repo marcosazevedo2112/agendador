ea10497f8b8a44bd283cdf279387a72511894b3b
Marcos Azevedo <marcosazevedo2112@gmail.com>
2026-09-14 23:58:50 -0300

    feat: add first verification to appointment availability

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -1,9 +1,15 @@
 import {Request, Response} from 'express';
 
+import appointmentService from '../services/appointment.service';
+
 const appointmentController = {
   getAllAppointments: async (req: Request, res: Response) => {},
   createAppointment: async (req: Request, res: Response) => {},
-  checkAvailability: async (req: Request, res: Response) => {},
+  checkAvailability: async (req: Request, res: Response) => {
+    const requestDate = res.locals.validatedQuery.date;
+    const isAvailable = await appointmentService.checkAvailability(requestDate);
+    return res.status(200).json({available: isAvailable});
+  },
 };
 
 export default appointmentController;
--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,3 +1,10 @@
-const appoimentService = {};
+import workDaysRepository from '../repositories/workDays.repository';
 
-export default appoimentService;
+const appointmentService = {
+  checkAvailability: (date: string) => {
+    return workDaysRepository.checkWorkDay(date);
+    //TODO: Implement the logic to check if a day is fully booked based on the appointments in the database.
+  },
+};
+
+export default appointmentService;
