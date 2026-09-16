07cde941c47f72c94a1a69d096b09b9c1722a922
Marcos <47260061+marcosazevedo2112@users.noreply.github.com>
2026-09-16 09:13:08 -0300

    feat: add appointment persistence and API

---
+++ src/controllers/appointment.controller.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from 'express';
+
+const appointmentController = {
+  getAllAppointments: async (req: Request, res: Response) => {},
+  createAppointment: async (req: Request, res: Response) => {},
+  checkAvailability: async (req: Request, res: Response) => {},
+};
+
+export default appointmentController;
